6eb28f02c0f8ba954bb97be88703ba3cdef4ea2f
Francis <thyeem@gmail.com>
2020-09-30 19:49:52 +0900

    performance improved

--- app/Main.hs
+++ app/Main.hs
@@ -1,54 +1,53 @@
 module Main where
 
-import           Control.Monad
 import           Control.DeepSeq
 import           Control.Parallel               ( par
                                                 , pseq
                                                 )
+import           Data.List                      ( find )
+import           Data.List.Split                ( chunksOf )
+import           Data.Maybe                     ( catMaybes )
 import qualified Data.ByteString.Char8         as C
 import qualified Data.ByteString.Base16        as H
-import           Data.List.Split                ( chunksOf )
 import qualified Crypto.Hash.SHA256            as S
 
-
--- | Number of chunks you want
-pieces :: Int
-pieces = 100000
+-- Here assume that only characters below to be used in a preimage
+-- ['0' .. '9']
+-- Search space will be tremendously increased when using like:
+-- ['A' .. 'Z'] <> ['a' .. 'z'] <> ['0' .. '9']
 
 -- | Preimage solution
 key :: String
 key = "12345678"
 
--- | Characters to be used in a preimage
-chars :: String
-chars = ['0' .. '9']
+-------------------------------------------------------------------
+-- | Number of chunks
+-- How many search zones would you have? 
+chunks :: Integer
+chunks = round . sqrt $ 10 ^ length key
 
--- Search space will be tremendously increased when using like:
--- chars = ['A' .. 'Z'] <> ['a' .. 'z'] <> ['0' .. '9']
-
--- | Split the whole search spaces by number of pieces
-chunks :: [[Integer]]
-chunks = chunksOf pieces [1 .. 10 ^ length key]
+-- | Size of each step
+unit :: Integer
+unit = fromIntegral (10 ^ length key) `div` chunks
 
 -- | Image: hash value to find
 image :: C.ByteString
 image = S.hash . C.pack $ key
 
 -- | Brute Force attack
-bruteforce :: C.ByteString -> [Integer] -> Bool
-bruteforce image searchSpace =
-  or ((image ==) . S.hash . C.pack . show <$> searchSpace)
+bruteforce :: Integer -> Maybe C.ByteString
+bruteforce index = find (== image) $ S.hash . C.pack . show <$> domain
+  where domain = [unit * index .. unit * succ index]
+
+-- | Solve the problem
+main :: IO ()
+main | null found = putStrLn "Not Found"
+     | otherwise  = putStrLn $ "Found: " <> (C.unpack . H.encode $ head found)
+  where found = catMaybes $ bruteforce <%> [0 .. chunks]
 
 -- | Parallel map using deepseq, par and pseq
-pmap :: (NFData a, NFData b) => (a -> b) -> [a] -> [b]
-pmap f []       = []
-pmap f (x : xs) = y `par` ys `pseq` (y : ys) where
+(<%>) :: (NFData a, NFData b) => (a -> b) -> [a] -> [b]
+f <%> []       = []
+f <%> (x : xs) = y `par` ys `pseq` (y : ys) where
   y  = force $ f x
-  ys = pmap f xs
-
-main :: IO ()
-main
-  | or $ bruteforce image `pmap` chunks
-  = putStrLn $ "Found: " <> (C.unpack . H.encode $ image)
-  | otherwise
-  = putStrLn "Not Found"
+  ys = f <%> xs
